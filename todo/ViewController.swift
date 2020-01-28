72afadc8fcc175e039d42c833ab1b306d1bf404d
Anatoliy Odinetskiy <odinaa92@mail.ru>
2020-01-28 15:57:23 +0700

    Initial Commit

---
+++ todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by Анатолий on 28/01/2020.
+//  Copyright © 2020 Anatoliy Odinetskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
