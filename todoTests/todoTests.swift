9ff761cb9ada0c76b43fba2dc1ea221eec2e9d77
Anatoliy Odinetskiy <odinaa92@mail.ru>
2020-03-12 23:56:01 +0700

    tests for UIColor extension

--- todoTests/todoTests.swift
+++ todoTests/todoTests.swift
@@ -2,7 +2,7 @@
 //  todoTests.swift
 //  todoTests
 //
-//  Created by Анатолий on 28/01/2020.
+//  Created by Анатолий on 12/03/2020.
 //  Copyright © 2020 Anatoliy Odinetskiy. All rights reserved.
 //
 
@@ -10,68 +10,86 @@
 import XCTest
 
 class todoTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
+    
+    func testHexWhiteColor() {
+        testColor(hex: 0xFFFFFF)
     }
-
-    override func tearDown() {
-        super.tearDown()
+    
+    func testHexBlackColor() {
+        testColor(hex: 0x000000)
     }
-/*
-    func testNotificationEveryDay() {
-        let authorizationExpectation = self.expectation(description: "authorization")
-        notifications.authorization() {
-            _, _ in
-            authorizationExpectation.fulfill()
-        }
-        waitForExpectations(timeout: 10, handler: nil)
-        
-        let id = getID()
-        var date = createDate(from: "02/01/2020 13:30:30")
-        let calendar = Calendar.autoupdatingCurrent
-        let dateComponents = calendar.dateComponents([.day, .month, .year, .hour, .minute], from: date)
-        var components = DateComponents()
-        components.hour = dateComponents.hour
-        components.minute = dateComponents.minute
-        let createExpectation = self.expectation(description: "create")
-        self.notifications.create(identifier: id, body: "", dateInfo: components, repeats: true, completion: {
-            _ in
-            createExpectation.fulfill()
-        })
-        waitForExpectations(timeout: 10, handler: nil)
-        
-        let notificationsExpectation = self.expectation(description: "notifications")
-        var notifications: [UNNotificationRequest] = []
-        self.notifications.center.getPendingNotificationRequests {
-            arr in
-            notifications = arr
-            notificationsExpectation.fulfill()
-        }
-        waitForExpectations(timeout: 10, handler: nil)
-        
-        let notification = notifications.first(where: { $0.identifier == id })
-        let trigger = notification!.trigger as! UNCalendarNotificationTrigger
-        let nextDate = trigger.nextTriggerDate()
-        let nextDateComponents = calendar.dateComponents([.day, .month, .year], from: nextDate!)
-
-        date.addTimeInterval(3600 * 24)
-        let nextDayComponents = calendar.dateComponents([.day, .month, .year], from: date)
-        XCTAssert(nextDateComponents == nextDayComponents)
+    
+    func testHexRedColor() {
+        testColor(hex: 0xFF0000)
     }
-
-    private func getID() -> String {
-        let id = UUID().uuidString
-        ids.append(id)
-        return id
+    
+    func testHexGreenColor() {
+        testColor(hex: 0x00FF00)
     }
-
-    private func createDate(from: String) -> Date {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MM/dd/yyyy HH:mm:ss"
-        formatter.timeZone = TimeZone(secondsFromGMT: 7 * 3600)
-        let date = formatter.date(from: from)!
-        return date
+    
+    func testHexBlueColor() {
+        testColor(hex: 0x0000FF)
     }
- */
+    
+    func testHexColor() {
+        testColor(hex: 0x123456)
+    }
+    
+    func testLighterBlackColor() {
+        testLighter(red: 0, green: 0, blue: 0, alpha: 1, diff: 0.1)
+    }
+    
+    func testLighterWhiteColor() {
+        testLighter(red: 1, green: 1, blue: 1, alpha: 1, diff: 0.1)
+    }
+    
+    func testLighterColor() {
+        testLighter(red: 0.1, green: 0.2, blue: 0.3, alpha: 1, diff: 0.1)
+    }
+    
+    func testInitWhiteColor() {
+        let color = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        let colorT = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+        XCTAssert(color == colorT)
+    }
+    
+    func testInitBlackColor() {
+        let color = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        let colorT = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        XCTAssert(color == colorT)
+    }
+    
+    func testInitRedColor() {
+        let color = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        let colorT = UIColor(red: 255, green: 0, blue: 0, alpha: 1)
+        XCTAssert(color == colorT)
+    }
+    
+    func testInitGreenColor() {
+        let color = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+        let colorT = UIColor(red: 0, green: 255, blue: 0, alpha: 1)
+        XCTAssert(color == colorT)
+    }
+    
+    func testInitBlueColor() {
+        let color = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        let colorT = UIColor(red: 0, green: 0, blue: 255, alpha: 1)
+        XCTAssert(color == colorT)
+    }
+    
+    private func testColor(hex: Int32) {
+        let color = UIColor(hex6: hex)
+        let rgb = color.rgb
+        XCTAssert(hex == rgb)
+    }
+    
+    private func testLighter(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat, diff: CGFloat) {
+        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        let lighterColor = UIColor(red: min(red + diff, 1.0),
+                                   green: min(green + diff, 1.0),
+                                   blue: min(blue + diff, 1.0),
+                                   alpha: alpha)
+        let lighterColorT = color.lighter(diff: diff)
+        XCTAssert(lighterColor == lighterColorT)
+    }
 }
